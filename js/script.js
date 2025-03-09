aa9511fecc427804a83f58adb964c7654b961fcd
Dhokai Raj B <rajdhokai2000@gmail.com>
2025-03-09 13:39:05 +0530

    Add Slider

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove("active");
+    if (i === index) {
+      slide.classList.add("active");
+    }
+  });
+}
+
+function nextSlide() {
+  slideIndex = (slideIndex + 1) % slides.length;
+  showSlide(slideIndex);
+}
+
+function prevSlide() {
+  slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+  showSlide(slideIndex);
+}
+
+// Auto-slide every 3 seconds
+setInterval(nextSlide, 3000);
